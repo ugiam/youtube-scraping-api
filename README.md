7058643bc8c003fbeeb6b8914683609ff700509f
ugiam <ugi.abdul98@gmail.com>
2025-07-08 11:58:45 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# youtube-scraping-api
+This repository was originated from https://github.com/melvinchia3636/YouTube-Scraping-API
+I try to fork the repo but the main script is deleted and the repository structure is completely different
---
+++ decorators.py
@@ -0,0 +1,10 @@
+import inspect
+
+def custom_property(f):
+    def wrapper(self, *args, **kwargs):
+        if self._is_builtin_called and f.__name__ in self._static_properties:
+            return self._static_properties[f.__name__]
+        if not self._has_generated:
+            self.parseData()
+        return f(self, *args, **kwargs)
+    return property(wrapper, doc=f.__doc__)
